43c5c9bfdebc1003328aef3c5c00a00c02fd376a
John Vicenzi <john.vicenzi@simplybusiness.com>
2023-05-18 13:38:44 -0400

    removed player1_points

--- ruby/tennis.rb
+++ ruby/tennis.rb
@@ -31,11 +31,11 @@ class TennisGame1
     return "Deuce" if duce?
     return advantage_or_win if advantage_or_win?
     return tied_result if point_difference == 0
-    to_result(@player1_points) + "-" + to_result(@player2_points)
+    to_result(@player1.points) + "-" + to_result(@player2_points)
   end
 
   def advantage_or_win?
-    @player1_points>=4 or @player2_points>=4
+    @player1.points>=4 or @player2_points>=4
   end
 
   def advantage_or_win
@@ -47,7 +47,7 @@ class TennisGame1
   end
 
   def duce?
-    (@player1_points >= 3 || @player2_points >= 3) && point_difference == 0
+    (@player1.points >= 3 || @player2_points >= 3) && point_difference == 0
   end
 
   def advantage
@@ -55,7 +55,7 @@ class TennisGame1
   end
 
   def point_difference
-    @player1_points-@player2_points
+    @player1.points-@player2_points
   end
 
   def tied_result
@@ -63,7 +63,7 @@ class TennisGame1
       0 => "Love-All",
       1 => "Fifteen-All",
       2 => "Thirty-All",
-    }[@player1_points]
+    }[@player1.points]
   end
 
   def to_result(point_score)
